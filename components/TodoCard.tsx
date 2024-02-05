92497822e90c96a4fab86a320922ea8a65b3f468
ABDUL RAHAMAN <36rahaman@gmail.com>
2024-02-05 17:20:21 +0530

    chore: functionality added

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import {
@@ -20,8 +23,7 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { keys } from "@/apis/query-keys";
 import { deleteTodo, editTodo } from "@/apis/todos/mutations";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function TodoCard({
   item,
@@ -61,11 +63,12 @@ export default function TodoCard({
     },
   });
 
-  console.log(item);
-  const [input, setInput] = useState<string>();
+  const [input, setInput] = useState<string>("");
+  const [isDialogOpen, setIsDialogOpen] = useState<Boolean>(false);
+
   return (
     <div>
-      <Card className=" w-full md:w-[400px] px-4 py-3 mb-4">
+      <Card className="w-full md:w-[400px] px-4 py-3 mb-4">
         <div className="flex justify-between items-center">
           <h1>{isAddTodoPending ? addTodoVariables.task : item.task}</h1>
           <Popover>
@@ -80,45 +83,54 @@ export default function TodoCard({
                   <div className="grid grid-cols-1 items-center gap-4">
                     <Dialog>
                       <DialogTrigger asChild>
-                        <Button variant="outline">
+                        <Button
+                          variant="outline"
+                          onClick={() => setIsDialogOpen(true)}
+                        >
                           <FaEdit size={18} className="text-yellow-400 mr-2" />
                           Edit Profile
                         </Button>
                       </DialogTrigger>
-                      <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                          <DialogTitle>Edit Todo</DialogTitle>
-                          <DialogDescription>
-                            Make changes to your Todo here. Click save when
-                            done.
-                          </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                          <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
-                              Todo
-                            </Label>
-                            <Input
-                              placeholder={item.task}
-                              id="name"
-                              className="col-span-3"
-                              onChange={(e) => {
-                                setInput(e.target.value);
-                              }}
-                            />
+                      {isDialogOpen && (
+                        <DialogContent className="sm:max-w-[425px]">
+                          <DialogHeader>
+                            <DialogTitle>Edit Todo</DialogTitle>
+                            <DialogDescription>
+                              Make changes to your Todo here. Click save when
+                              done.
+                            </DialogDescription>
+                          </DialogHeader>
+                          <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                              <Label htmlFor="name" className="text-right">
+                                Todo
+                              </Label>
+                              <Input
+                                placeholder={item.task}
+                                id="name"
+                                className="col-span-3"
+                                onChange={(e) => {
+                                  setInput(e.target.value);
+                                }}
+                              />
+                            </div>
                           </div>
-                        </div>
-                        <DialogFooter>
-                          <Button
-                            type="submit"
-                            onClick={() => {
-                              EditTodoMutation({ id: item.id, task: input });
-                            }}
-                          >
-                            Save changes
-                          </Button>
-                        </DialogFooter>
-                      </DialogContent>
+                          <DialogFooter>
+                            <Button
+                              type="submit"
+                              onClick={() => {
+                                EditTodoMutation({
+                                  id: item.id,
+                                  task: input,
+                                });
+                                setIsDialogOpen(false); // Close the dialog
+                              }}
+                            >
+                              Save changes
+                            </Button>
+                          </DialogFooter>
+                        </DialogContent>
+                      )}
                     </Dialog>
                     <Button
                       onClick={() => {
