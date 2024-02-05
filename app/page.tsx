30f8c545b042898f4fe7f6c54a7ac06ee72d72ec
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-02-05 22:16:34 +0530

    chore: main page cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,23 @@
 "use client";
 
-import { login, logout } from "@/apis/auth";
 import { keys } from "@/apis/query-keys";
 import { addTodo, getAllTodos } from "@/apis/todos";
 import TodoCard from "@/components/TodoCard";
 import TodoCardSkeleton from "@/components/TodoCardSkeleton";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { isAuthenticated } from "@/lib/auth";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-import { Card } from "@/components/ui/card";
 const Todo = () => {
   const queryClient = useQueryClient();
   const [input, setInput] = useState<string>();
 
-  const {
-    isPending: isLoginPending,
-    variables: loginVariables,
-    mutate: loginMutation,
-  } = useMutation({
-    mutationKey: keys("/api/users/login", "post").main(),
-    mutationFn: (userDetails: any) => login(userDetails),
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({
-        queryKey: keys("/api/todos", "get").main(),
-      });
-    },
-  });
-
-  const {
-    isPending: isLogoutPending,
-    variables: logoutVariables,
-    mutate: logoutMutation,
-  } = useMutation({
-    mutationKey: keys("/api/users/logout", "post").main(),
-    mutationFn: () => logout(),
-    onSuccess: async () => {
-      await queryClient.setQueryData(keys("/api/todos", "get").main(), []);
-    },
-  });
-
   const { data: todoData, isLoading } = useQuery({
     queryKey: keys("/api/todos", "get").main(),
     queryFn: getAllTodos,
+    enabled: Boolean(async () => await isAuthenticated()),
   });
 
   const {
@@ -61,35 +36,6 @@ const Todo = () => {
 
   return (
     <div className="flex flex-col items-center pt-[50px] min-h-screen bg-gray-100 dark:bg-gray-900 p-4 md:p-10">
-      <Button
-        onClick={() =>
-          loginMutation({
-            email: "36rahaman@gmail.com",
-            password: "123456",
-          })
-        }
-      >
-        Login
-      </Button>
-
-      {/* <main className="w-full max-w-md mx-auto flex justify-around">
-        <Button
-          onClick={() =>
-            loginMutation({
-              email: "36rahaman@gmail.com",
-              password: "123456",
-            })
-          }
-        >
-          Login
-        </Button>
-        {/* <Button onClick={() => addTodoMutation({ task: "akhil" })}>
-          AddTodo
-        </Button>
-        <Button onClick={() => logoutMutation()}>Logout</Button> 
-      </main>
-      */}
-
       <main className="flex gap-4 mb-[20px] h-auto w-full md:md:w-[400px] ">
         <Input
           className="py-6"
--- components/TodoCardSkeleton.tsx
+++ components/TodoCardSkeleton.tsx
@@ -2,9 +2,9 @@ import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 function TodoCardSkeleton() {
   return (
-    <div className="flex flex-col items-center space-y-6">
+    <div className="flex flex-col items-center space-y-4">
       {Array.from({ length: 4 }, (_, index) => (
-        <Card key={index} className="w-full md:w-[400px] px-4 py-4 mb-4 ">
+        <Card key={index} className="w-full md:w-[400px] px-4 py-4">
           <div className="flex items-center justify-between">
             <div className="w-full md:w-[300px] space-y-2">
               <Skeleton className="h-4 w-full" />
