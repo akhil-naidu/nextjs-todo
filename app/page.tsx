c7c73fe1f23c892e775f151e40cc28608ae4159e
ABDUL RAHAMAN <36rahaman@gmail.com>
2024-02-05 17:07:24 +0530

    chore: add, update, delete functionality completed

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,11 @@ import TodoCard from "@/components/TodoCard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 
 const Todo = () => {
   const queryClient = useQueryClient();
+  const [input, setInput] = useState<string>();
 
   const {
     isPending: isLoginPending,
@@ -58,26 +60,43 @@ const Todo = () => {
 
   return (
     <div className="flex flex-col items-center pt-[50px] min-h-screen bg-gray-100 dark:bg-gray-900">
-      {/* <main className="w-full max-w-md mx-auto flex justify-around">
+      <main className="w-full max-w-md mx-auto flex justify-around">
         <Button
           onClick={() =>
             loginMutation({
-              email: "kaparapu.akhilnaidu@gmail.com",
-              password: "iwillhack",
+              email: "36rahaman@gmail.com",
+              password: "123456",
             })
           }
         >
           Login
         </Button>
-        <Button onClick={() => addTodoMutation({ task: "akhil" })}>
+        {/* <Button onClick={() => addTodoMutation({ task: "akhil" })}>
           AddTodo
         </Button>
-        <Button onClick={() => logoutMutation()}>Logout</Button>
-      </main> */}
+        <Button onClick={() => logoutMutation()}>Logout</Button> */}
+      </main>
 
       <main className="flex gap-4 mb-[20px] h-auto w-full md:md:w-[400px] ">
-        <Input className="py-6" type="text" placeholder="Enter Todo" />
-        <Button className="py-6">Add Todo</Button>
+        <Input
+          className="py-6"
+          type="text"
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          value={input}
+          placeholder="Enter Todo"
+          required={true}
+        />
+        <Button
+          className="py-6"
+          onClick={() => {
+            addTodoMutation({ task: input });
+            setInput("");
+          }}
+        >
+          Add Todo
+        </Button>
       </main>
       <div>
         {isAddTodoPending && (
@@ -87,16 +106,16 @@ const Todo = () => {
         )}
         {todoData?.map((todo: any) => {
           return (
-            <div
-              className="py-2 px-4 bg-gray-200 w-60  m-4 text-center rounded-md"
-              key={todo.id}
-            >
-              {todo.task}
+            <div key={todo.id}>
+              <TodoCard
+                item={todo}
+                isAddTodoPending={isAddTodoPending}
+                addTodoVariables={addTodoVariables}
+              />
             </div>
           );
         })}
       </div>
-      <TodoCard />
     </div>
   );
 };
--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -18,12 +18,56 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 import { FaEdit } from "react-icons/fa";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
-export default function TodoCard() {
+import { keys } from "@/apis/query-keys";
+import { deleteTodo, editTodo } from "@/apis/todos/mutations";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function TodoCard({
+  item,
+  addTodoVariables,
+  isAddTodoPending,
+}: {
+  item: any;
+  addTodoVariables: any;
+  isAddTodoPending: any;
+}) {
+  const queryClient = useQueryClient();
+  const {
+    isPending: isDeleteTodoPending,
+    variables: DeleteTodoVariables,
+    mutate: DeleteTodoMutation,
+  } = useMutation({
+    mutationKey: keys(`/api/users/todos/`, "delete").main(),
+    mutationFn: (todoData: any) => deleteTodo(todoData),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: keys("/api/todos", "get").main(),
+      });
+    },
+  });
+
+  const {
+    isPending: isEditTodoPending,
+    variables: EditTodoVariables,
+    mutate: EditTodoMutation,
+  } = useMutation({
+    mutationKey: keys(`/api/users/todos/`, "patch").main(),
+    mutationFn: (obj: any) => editTodo(obj.id, { task: obj.task }),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: keys("/api/todos", "get").main(),
+      });
+    },
+  });
+
+  console.log(item);
+  const [input, setInput] = useState<string>();
   return (
     <div>
       <Card className=" w-full md:w-[400px] px-4 py-3 mb-4">
         <div className="flex justify-between items-center">
-          <h1>Todo item</h1>
+          <h1>{isAddTodoPending ? addTodoVariables.task : item.task}</h1>
           <Popover>
             <PopoverTrigger asChild>
               <div className="cursor-pointer">
@@ -54,16 +98,33 @@ export default function TodoCard() {
                             <Label htmlFor="name" className="text-right">
                               Todo
                             </Label>
-                            <Input id="name" className="col-span-3" />
+                            <Input
+                              placeholder={item.task}
+                              id="name"
+                              className="col-span-3"
+                              onChange={(e) => {
+                                setInput(e.target.value);
+                              }}
+                            />
                           </div>
                         </div>
                         <DialogFooter>
-                          <Button type="submit">Save changes</Button>
+                          <Button
+                            type="submit"
+                            onClick={() => {
+                              EditTodoMutation({ id: item.id, task: input });
+                            }}
+                          >
+                            Save changes
+                          </Button>
                         </DialogFooter>
                       </DialogContent>
                     </Dialog>
-                    <Button>
-                      {" "}
+                    <Button
+                      onClick={() => {
+                        DeleteTodoMutation(item.id);
+                      }}
+                    >
                       <FaEdit size={18} className="text-red-400 mr-2" /> Delete
                       Todo
                     </Button>
