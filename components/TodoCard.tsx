3b471ddd0d2ad4a2e060993b94aabb2a3e81a096
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-02-06 12:35:12 +0530

    init: components from taxonomy

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { keys } from "@/apis/query-keys";
-import { deleteTodo, editTodo } from "@/apis/todos/mutations";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { keys } from '@/apis/query-keys';
+import { deleteTodo, editTodo } from '@/apis/todos/mutations';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import {
   Dialog,
   DialogContent,
@@ -12,18 +12,18 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
+} from '@/components/ui/dialog';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import { FaEdit } from "react-icons/fa";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
+} from '@/components/ui/popover';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { FaEdit } from 'react-icons/fa';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 export default function TodoCard({
   item,
@@ -40,11 +40,11 @@ export default function TodoCard({
     variables: DeleteTodoVariables,
     mutate: deleteTodoMutation,
   } = useMutation({
-    mutationKey: keys(`/api/todos/`, "delete").detail(item.id),
+    mutationKey: keys(`/api/todos/`, 'delete').detail(item.id),
     mutationFn: (todoData: any) => deleteTodo(todoData),
     onSuccess: async () => {
       await queryClient.invalidateQueries({
-        queryKey: keys("/api/todos", "get").main(),
+        queryKey: keys('/api/todos', 'get').main(),
       });
     },
   });
@@ -54,45 +54,45 @@ export default function TodoCard({
     variables: EditTodoVariables,
     mutate: EditTodoMutation,
   } = useMutation({
-    mutationKey: keys(`/api/users/todos/`, "patch").main(),
+    mutationKey: keys(`/api/users/todos/`, 'patch').main(),
     mutationFn: (obj: any) => editTodo(obj.id, { task: obj.task }),
     onSuccess: async () => {
       await queryClient.invalidateQueries({
-        queryKey: keys("/api/todos", "get").main(),
+        queryKey: keys('/api/todos', 'get').main(),
       });
     },
   });
 
-  const [input, setInput] = useState<string>("");
+  const [input, setInput] = useState<string>('');
   const [isDialogOpen, setIsDialogOpen] = useState<Boolean>(false);
 
   return (
-    <div className="w-full md:w-[400px] ">
-      <Card className="w-full md:w-[400px] px-4 py-4 mb-4 bg-gray-200">
-        <div className="flex justify-between items-center">
+    <div className='w-full md:w-[400px] '>
+      <Card className='w-full md:w-[400px] px-4 py-4 mb-4 bg-gray-200'>
+        <div className='flex justify-between items-center'>
           <h1>{item.task}</h1>
           <Popover>
             <PopoverTrigger asChild>
-              <div className="cursor-pointer">
+              <div className='cursor-pointer'>
                 <BsThreeDotsVertical size={18} />
               </div>
             </PopoverTrigger>
-            <PopoverContent className="w-45 bg-gray-100">
-              <div className="grid gap-4">
-                <div className="grid gap-2">
-                  <div className="grid grid-cols-1 items-center gap-4">
+            <PopoverContent className='w-45 bg-gray-100'>
+              <div className='grid gap-4'>
+                <div className='grid gap-2'>
+                  <div className='grid grid-cols-1 items-center gap-4'>
                     <Dialog>
                       <DialogTrigger asChild>
                         <Button
-                          variant="outline"
+                          variant='outline'
                           onClick={() => setIsDialogOpen(true)}
                         >
-                          <FaEdit size={18} className="text-yellow-400 mr-2" />
+                          <FaEdit size={18} className='text-yellow-400 mr-2' />
                           Edit Todo
                         </Button>
                       </DialogTrigger>
                       {isDialogOpen && (
-                        <DialogContent className="sm:max-w-[425px]">
+                        <DialogContent className='sm:max-w-[425px]'>
                           <DialogHeader>
                             <DialogTitle>Edit Todo</DialogTitle>
                             <DialogDescription>
@@ -100,15 +100,15 @@ export default function TodoCard({
                               done.
                             </DialogDescription>
                           </DialogHeader>
-                          <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                              <Label htmlFor="name" className="text-right">
+                          <div className='grid gap-4 py-4'>
+                            <div className='grid grid-cols-4 items-center gap-4'>
+                              <Label htmlFor='name' className='text-right'>
                                 Todo
                               </Label>
                               <Input
                                 placeholder={item.task}
-                                id="name"
-                                className="col-span-3"
+                                id='name'
+                                className='col-span-3'
                                 onChange={(e) => {
                                   setInput(e.target.value);
                                 }}
@@ -117,7 +117,7 @@ export default function TodoCard({
                           </div>
                           <DialogFooter>
                             <Button
-                              type="submit"
+                              type='submit'
                               onClick={() => {
                                 EditTodoMutation({
                                   id: item.id,
@@ -137,7 +137,7 @@ export default function TodoCard({
                         deleteTodoMutation(item.id);
                       }}
                     >
-                      <FaEdit size={18} className="text-red-400 mr-2" /> Delete
+                      <FaEdit size={18} className='text-red-400 mr-2' /> Delete
                       Todo
                     </Button>
                   </div>
--- components/TodoCardSkeleton.tsx
+++ components/TodoCardSkeleton.tsx
@@ -1,16 +1,16 @@
-import { Card } from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
+import { Card } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 function TodoCardSkeleton() {
   return (
-    <div className="flex flex-col items-center space-y-4">
+    <div className='flex flex-col items-center space-y-4'>
       {Array.from({ length: 4 }, (_, index) => (
-        <Card key={index} className="w-full md:w-[400px] px-4 py-4">
-          <div className="flex items-center justify-between">
-            <div className="w-full md:w-[300px] space-y-2">
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-full" />
+        <Card key={index} className='w-full md:w-[400px] px-4 py-4'>
+          <div className='flex items-center justify-between'>
+            <div className='w-full md:w-[300px] space-y-2'>
+              <Skeleton className='h-4 w-full' />
+              <Skeleton className='h-4 w-full' />
             </div>
-            <Skeleton className="h-8 w-8 rounded-full" />
+            <Skeleton className='h-8 w-8 rounded-full' />
           </div>
         </Card>
       ))}
