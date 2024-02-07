129c835558dbeba2fed874bfe2d9d66ed4b9e91d
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-07 14:53:28 +0530

    Final Todo (#9)
    * init: login page
    
    * chore:ui updated
    
    * chore: added login ui
    
    * chore: added login logic
    
    * updated: background color changed
    
    * updated: make responsive ui
    
    * init: navbar
    
    * chore: skeleton added
    
    * chore: add, update, delete functionality completed
    
    * chore: functionality added
    
    * chore: frontend changes done
    
    * chore: adding protected routes by client side and client side HOC
    
    * chore: added protected routes using middleware
    
    * chore: main page cleanup
    
    * chore: updating the query keys
    
    * init: components from taxonomy
    
    * init: main page from taxonomy
    
    * chore: updated the home page ui
    
    * init: dashboard
    
    * chore: billing and settings page
    
    * Merge branch 'dev' into ui-update-mp
    
    * chore: added login page
    
    * chore: todo get functionality
    
    * chore: updated todo and settings page
    
    * chore: added ui and functionality for add a new todo
    
    * chore: added ui and functionality for edit todo
    
    * chore: updating todo button name
    
    * update: replacing shadcn with contentql
    
    * chore: Authenticated users when clicked on login should navigate to Dashboard
    
    * remove: console warnings
    
    * chore: disable unwanted links nav
    
    * chore: updated a new footer
    
    * chore: added middleware protected routes
    
    * chore: checking the logged in user using payload api
    
    * chore: updated settings page
    
    * chore: signup page
    
    * chore: updated the app dir folder structure to achieve the authotization
    
    * unwnted files removed
    
    * chore: updated the authorizatioon
    
    * chore:
    structuring billing
    Co-authored-by: ManikantaPotnuru9176 <ManikantaPotnuru9176@users.noreply.github.com>
    
    * chore: login page access
    
    * chore: dynamic navbar for account dropdown
    
    ---------
    
    Co-authored-by: ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
    Co-authored-by: jagadeeesh507 <jagadeeshm778@gmail.com>
    Co-authored-by: ABDUL RAHAMAN <36rahaman@gmail.com>
    Co-authored-by: ManikantaPotnuru9176 <110469998+ManikantaPotnuru9176@users.noreply.github.com>

--- apis/todos/mutations.ts
+++ apis/todos/mutations.ts
@@ -1,8 +1,28 @@
 import http from '@/lib/axios';
 
-export const addTodo = async (data: any) => {
+export const addTodo = async (data: Object) => {
   try {
     const res = await http.post('/api/todos', data);
+
+    return res.data.doc;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteTodo = async (id: string) => {
+  try {
+    const res = await http.delete(`/api/todos/${id}`);
+
+    return res.data.doc;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editTodo = async (id: string, data: Object) => {
+  try {
+    const res = await http.put(`/api/todos/${id}`, data);
 
     return res.data.doc;
   } catch (error) {
